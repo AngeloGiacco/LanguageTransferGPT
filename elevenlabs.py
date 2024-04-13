54d2c0d78d640d3f40c7c74d368502d34946c905
Giacco, Angelo <giacco.angelo.s@gmail.com>
2024-04-13 18:03:55 +0200

    update readme

---
+++ elevenlabs.py
@@ -0,0 +1,3 @@
+#just saving this here for now
+
+supported = ["English","Chinese","Spanish","Hindi","Portuguese","French","German","Japanese","Arabic","Russian","Korean","Indonesian","Italian","Dutch","Turkish","Polish","Swedish","Filipino","Malay","Romanian","Ukrainian","Greek","Czech","Danish","Finnish","Bulgarian","Croatian","Slovak","Tamil"]
