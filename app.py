a491c1b96aa2965eb5483a6db7ff2b4f0cded031
Giacco, Angelo <giacco.angelo.s@gmail.com>
2024-05-03 23:04:00 +0200

    feat: user emojis and handle new lesson state

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from helpers import *
 
 lesson_started_key = "lesson_started"
 
@@ -16,8 +17,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-st.title("Poppa")
-st.header("Language lessons on demand!")
+st.title("👴 poppa - infinite language lessons")
 
 with st.form("my_form"):
     # Define the available languages and levels
@@ -32,40 +32,56 @@ with st.form("my_form"):
             "Expert",
         ]
         target_language = st.selectbox(
-            "Select the language you want to learn:", languages, key="target_language"
+            "Select the language you want to learn:",
+            supported_languages,
+            key="target_language",
+            index=1,
         )
         current_language = st.selectbox(
-            "Select your current language:", languages, key="native_language"
+            "Select your current language:",
+            supported_languages,
+            key="native_language",
+            index=0,
         )
         level = st.selectbox(
-            "Select your level in the language you're learning:", levels, key="level"
+            "Select your level in the language you're learning:",
+            levels,
+            key="level",
+            index=1,
         )
-        topic = st.text_input("Enter the topic you want to learn about:", key="topic")
+        topic = st.text_input("Want to learn something in particular?", key="topic")
 
         submit = (
             st.form_submit_button("Start learning!")
-            if st.session_state[lesson_started_key]
+            if not st.session_state[lesson_started_key]
             else st.form_submit_button("Start new lesson!")
         )
-user_input = None
 if submit:
-    system_message = f"You are a language tutor helping a {level} learner of {target_language} whose current language is {current_language}. The topic for the lesson is: {topic}. Provide an engaging and interactive lesson on this topic."
+    st.session_state.chat_history = []
     st.session_state.chat_history.append(("assistant", "I am generating a lesson"))
     st.session_state[lesson_started_key] = True
 
+
+def get_avatar(user):
+    if user == "assistant":
+        return "👴"
+    else:
+        return "🙋🏻‍♀️"
+
+
 for user, text in st.session_state.chat_history:
-    with st.chat_message(user):
+    with st.chat_message(user, avatar=get_avatar(user)):
         st.markdown(text)
 
 if st.session_state[lesson_started_key]:
     if prompt := st.chat_input("enter what you want to say!"):
 
         st.session_state.chat_history.append(("user", prompt))
-        with st.chat_message("user"):
+        with st.chat_message("user", avatar=get_avatar("user")):
             st.markdown(prompt)
 
-        response = f"You want a lesson in {st.session_state.native_language} for {st.session_state.target_language} at {st.session_state.level} level on {st.session_state.topic} and you said {prompt}"
+        response = f"<p>You want a lesson in {st.session_state.native_language} for {st.session_state.target_language} at {st.session_state.level} level on {st.session_state.topic} and you said {prompt}</p>"
 
         st.session_state.chat_history.append(("assistant", response))
-        with st.chat_message("assistant"):
-            st.markdown(response)
+        with st.chat_message("assistant", avatar=get_avatar("assistant")):
+            st.markdown(response, unsafe_allow_html=True)
