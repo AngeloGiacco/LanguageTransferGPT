29343f2f3aea854194b8e9092a78a0c2dbab89fe
Giacco, Angelo <giacco.angelo.s@gmail.com>
2024-05-09 14:55:27 +0200

    delete unneeded files

--- elevenlabs.py
+++
@@ -1,3 +0,0 @@
-#just saving this here for now
-
-supported = ["English","Chinese","Spanish","Hindi","Portuguese","French","German","Japanese","Arabic","Russian","Korean","Indonesian","Italian","Dutch","Turkish","Polish","Swedish","Filipino","Malay","Romanian","Ukrainian","Greek","Czech","Danish","Finnish","Bulgarian","Croatian","Slovak","Tamil"]
